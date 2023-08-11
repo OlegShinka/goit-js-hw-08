3f86fec178fe4bc3042693084f4847c58b577d6f
OlegShinka <volkonxxl@gmail.com>
2023-08-11 21:32:40 +0300

    player01

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let time;
+const timePlay = function (data) {
+  time = data.seconds;
+  console.log(time);
+  localStorage.setItem('videoplayer-current-time', time);
+};
+player.on('timeupdate', throttle(timePlay, 1000));
+let current = localStorage.getItem('videoplayer-current-time');
+console.log('currentTime', current);
+player.setCurrentTime(current);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const refs = {
+  formEl: document.querySelector('.feedback-form'),
+  inputEl: document.querySelector('input'),
+  textEl: document.querySelector('textarea'),
+};
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.inputEl.addEventListener('input', onEmailInput);
+refs.textEl.addEventListener('input', onTextareaInput);
 
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(evt.currentTarget);
+}
+const inputData = {};
+
+let emailInput;
+
+function onEmailInput(evt) {
+  //   console.log(evt.currentTarget.value);
+  inputData.email = evt.target.value;
+  localStorage.setItem('input-email', JSON.stringify(inputData));
+
+  //
+}
+console.log(inputData);
+
+function onTextareaInput(evt) {
+  console.log(evt.target.value);
+  const textAreaInput = evt.currentTarget.value;
+  inputData.message = textAreaInput;
+}
